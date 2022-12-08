436679525ed39537b6b4ee8177355a170fd12ecb
Alexandru Chirita <alexandru.chirita@bearingpoint.com>
2022-12-08 17:33:30 +0200

    helm

--- src/main/java/com/example/demo2/Demo2Application.java
+++ src/main/java/com/example/demo2/Demo2Application.java
@@ -9,5 +9,6 @@ public class Demo2Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Demo2Application.class, args);
 	}
-
+	// TODO last check 0712
+	// https://www.alibabacloud.com/blog/kubernetes-configure-liveness-and-readiness-probes_594833
 }
